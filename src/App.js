42ff4f461d3fbe9b6c24a8977b3c0898046b4a53
HenryNguyen <nhtruong2@gmail.com>
2023-02-22 05:53:09 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import TodoFeature from "./features/Todo/pages";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Henry nguyen</p>
-            </header>
+            <TodoFeature />
         </div>
     );
 }
---
+++ src/features/Todo/pages/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import TodoList from '../components/TodoList';
+
+const TodoFeature = (props) => {
+    const todoList = [
+        {id : 1, title : "Eat"},
+        {id : 2, title : "Sleep"},
+        {id : 3, title : "Code"},
+    ]
+    return (
+        <div>
+            <h3>Todo List</h3>
+            <TodoList todoList={todoList}/>
+        </div>
+    );
+};
+
+export default TodoFeature;
