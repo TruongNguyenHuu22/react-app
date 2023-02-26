b5bd45c490f2bef298364eb01df81e953dd3a4df
HenryNguyen <nhtruong2@gmail.com>
2023-02-27 05:07:27 +0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import AlbumFeature from "./features/Album/pages";
 import TodoFeature from "./features/Todo/pages";
 
 function App() {
+    let activeClassName = "underline";
     return (
         <div className="App">
-            <TodoFeature />
-            {/* <AlbumFeature /> */}
+            Header
+            <p>
+                <NavLink
+                    to="/todo"
+                    className={({ isActive }) =>
+                        isActive ? activeClassName : undefined
+                    }
+                >
+                    Todo
+                </NavLink>
+            </p>
+            <p>
+                <NavLink
+                    to="/albums"
+                    className={({ isActive }) =>
+                        isActive ? activeClassName : undefined
+                    }
+                >
+                    Album
+                </NavLink>
+            </p>
+            <Routes>
+                <Route path="/todo" element={<TodoFeature />} />
+                <Route path="/albums" element={<AlbumFeature />} />
+            </Routes>
+            Footer
         </div>
     );
 }
